0dcaf5f35c48ac016d3c9cf5e8a0341d640080e0
Sannasichottu <sannasichottu@gmail.com>
2022-04-27 16:39:09 +0530

    blog-server

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from "cors";
-
+//import 'dotenv/config';
 const app = express ();
 app.use(cors());
 app.use(express.json());
 app.use("/api/user",router);  
 app.use("/api/blog",blogRouter);
 
-mongoose.connect('mongodb+srv://BlogApp:Dhosanjay7@clusters.k8lwf.mongodb.net/Blog?retryWrites=true&w=majority').then(() =>app.listen(5000)).then(()=>console.log("Connected To Database and Listening To Localhost 5000")).catch((err) => console.log(err));
+mongoose.connect(`mongodb+srv://BlogApp:Dhosanjay7@clusters.k8lwf.mongodb.net/Blog?retryWrites=true&w=majority`).then(() =>app.listen(500)).then(()=>console.log("Connected To Database and Listening To Localhost 5000")).catch((err) => console.log(err));
 
 
